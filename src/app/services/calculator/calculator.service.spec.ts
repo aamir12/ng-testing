e0439a5b4f87c6e7063ccde7c39a8844563298d8
aamir <aamiramir.khan@archetypegrowth.com>
2023-05-30 18:23:02 +0530

    add before each

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -3,10 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
+
+  let calculatorService:CalculatorService;
+  let mockLoggerService:any;
+  beforeEach(()=> {
+    mockLoggerService = jasmine.createSpyObj('LoggerService',['log']);
+    calculatorService = new CalculatorService(mockLoggerService);
+  })
   
   it('should add two numbers', () => {
-    const mockLoggerService = jasmine.createSpyObj('LoggerService',['log']);
-    const calculatorService = new CalculatorService(mockLoggerService);
+    
     let result = calculatorService.add(2,4);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
     expect(result).toBe(6);
@@ -14,8 +20,6 @@ describe('CalculatorService', () => {
   
   
   it('should subtract two numbers', () => {
-    const mockLoggerService = jasmine.createSpyObj('LoggerService',['log']);
-    const calculatorService = new CalculatorService(mockLoggerService);
     let result = calculatorService.subtract(4,2);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
     expect(result).toBe(2);
