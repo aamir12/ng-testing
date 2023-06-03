3ccd9ebe02c5a1dccfcca278d0a4aeac5b5563a7
aamir <aamiramir.khan@archetypegrowth.com>
2023-06-03 19:02:57 +0530

    update

--- src/app/components/posts/posts.component.spec.ts
+++ src/app/components/posts/posts.component.spec.ts
@@ -4,6 +4,8 @@ import { Post } from 'src/app/models/Post';
 import { PostService } from 'src/app/services/Post/post.service';
 import { PostsComponent } from './posts.component';
 import { Component, Input } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { PostComponent } from '../post/post/post.component';
 
 // class MockPostService {
 //   getPosts() {}
@@ -19,13 +21,13 @@ describe('Posts Component', () => {
   let mockPostService: any;
   let fixture : ComponentFixture<PostsComponent>
 
-  @Component({
-    selector:'app-post',
-    template:'<div></div>'
-  })
-  class FakePostComponent {
-    @Input() post!: Post;
-  }
+  // @Component({
+  //   selector:'app-post',
+  //   template:'<div></div>'
+  // })
+  // class FakePostComponent {
+  //   @Input() post!: Post;
+  // }
 
   beforeEach(() => {
     POSTS = [
@@ -49,7 +51,7 @@ describe('Posts Component', () => {
     mockPostService = jasmine.createSpyObj(['getPosts', 'deletePost']);
 
     TestBed.configureTestingModule({
-      declarations:[PostsComponent,FakePostComponent],
+      declarations:[PostsComponent,PostComponent],
       providers: [
         {
           provide: PostService,
@@ -69,6 +71,32 @@ describe('Posts Component', () => {
     expect(component.posts.length).toBe(3);
   });
 
+  it('should create child post element for each post', () => {
+    mockPostService.getPosts.and.returnValue(of(POSTS));
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement;
+    const postElements = debugElement.queryAll(By.css('.post'))
+    expect(postElements.length).toBe(POSTS.length);
+  })
+
+  it('should create exact same number of post component', () => {
+    mockPostService.getPosts.and.returnValue(of(POSTS));
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement;
+    const postElements = debugElement.queryAll(By.directive(PostComponent))
+    expect(postElements.length).toBe(POSTS.length);
+  });
+
+  it('should check exact post is passing to child component', () => {
+    mockPostService.getPosts.and.returnValue(of(POSTS));
+    fixture.detectChanges();
+    const postElementDEs = fixture.debugElement.queryAll(By.directive(PostComponent));
+    for(let i = 0; i< postElementDEs.length;i++) {
+      const postElement = postElementDEs[i].componentInstance as PostComponent;
+      expect(postElement.post.title).toEqual(POSTS[i].title);
+    }
+  })
+
   describe('delete', () => {
     beforeEach(() => {
       mockPostService.deletePost.and.returnValue(of(true));
@@ -77,20 +105,17 @@ describe('Posts Component', () => {
 
     it('should delete the selected Post from the posts', () => {
       component.delete(POSTS[1]);
-
       expect(component.posts.length).toBe(2);
     });
 
     it('should delete the actual selected Post in Posts', () => {
       component.delete(POSTS[1]);
-
       for (let post of component.posts) {
         expect(post).not.toEqual(POSTS[1]);
       }
     });
 
     it('should call the delete method in Post Service only once', () => {
-      //spyOn(mockPostService, 'deletePost').and.callThrough();
       component.delete(POSTS[1]);
       expect(mockPostService.deletePost).toHaveBeenCalledTimes(1);
     });
